f7fed50748b8e31d6272672b7d7a60c3bb62536f
Metehan <mac@mac-MacBook-Pro.local>
2023-02-09 12:48:41 +0300

    Enemy is added

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# import pygame
-
 import pygame
-
-
-
-# defined the RGB color code for the background
-
-background_colour = (139,0,0)
-
-
-
-# defined the size of the screen (width, height)
-
-screen = pygame.display.set_mode((600, 300))
-
-import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
@@ -33,8 +18,19 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+# Enemy
+enemyImage = pygame.image.load("enemy.png")
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0.3
+enemyY_change = 40
+
 def player(x,y):
     screen.blit(playerImage,(x,y))
+
+def enemy(x,y):
+    screen.blit(enemyImage,(x,y))
+
 # Game Loop
 running = True
 while running:
@@ -51,11 +47,25 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
+
+   # Checking for boundaries of spaceship so it doesn't go out of bounds
     playerX += playerX_change   
+
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+    
+    # Enemy Movement
+    enemyX += enemyX_change   
+    
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
 
     player(playerX,playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
