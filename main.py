399f0d059d323aeb8646a1f55858e59e6e93f420
Metehan <mac@mac-MacBook-Pro.local>
2023-02-09 13:49:06 +0300

    Score is added

--- main.py
+++ main.py
@@ -45,8 +45,16 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
-score = 0
+#Score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf',32)
 
+textX = 10
+textY = 10
+
+def show_score(x,y):
+    score = font.render("Score : " + str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
 def player(x,y):
     screen.blit(playerImage,(x,y))
 
@@ -111,10 +119,10 @@ while running:
         if collision:
             bulletY = 480
             bullet_state = "ready"
-            score += 1
+            score_value += 1
             enemyX[i] = random.randint(0,800)
             enemyY[i] = random.randint(50,150)
-            print(score)
+            print(score_value)
         enemy(enemyX[i], enemyY[i],i)
 
     # Bullet Movement
@@ -127,4 +135,5 @@ while running:
 
     
     player(playerX,playerY)
+    show_score(textX, textY)
     pygame.display.update()
